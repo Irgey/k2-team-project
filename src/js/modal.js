0d4aeb32631a1e18e4698c6ab3883e04f8221f4b
lysbrodya <110303676+lysbrodya@users.noreply.github.com>
2023-02-18 22:19:50 +0200

    js modal buttons

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,15 @@
 (() => {
   const refs = {
+    openModalBtnFlow: document.querySelector('[data-modal-open-flower]'),
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    closeOpenModalBtn: document.querySelector('[data-modal-close-open]'),
     modal: document.querySelector('[data-modal]'),
   };
-
+  refs.openModalBtnFlow.addEventListener('click', toggleModal);
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeOpenModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
