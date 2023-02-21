a3a2924777ffb390a845d25e37a585962208ecdb
Irgey <116591589+Irgey@users.noreply.github.com>
2023-02-21 17:12:16 +0100

    favicon path change

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // import './js/mobile-menu';
 import './js/modal';
 import './js/header-fixed';
+import './js/mobile-menu';
+import './js/close-mob-menu';
 import './js/header-change-color';
 import './js/slider';
 import './js/modal-ty';
 import './js/modal-subscribe';
-import './js/mobile-menu';
-import './js/close-mob-menu';
