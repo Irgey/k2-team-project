ad753d9f398dd914e3ab23e66949204f717bb8ec
Irgey <116591589+Irgey@users.noreply.github.com>
2023-02-21 23:00:54 +0100

    modal subscribe js fix

--- src/js/modal-subscribe.js
+++ src/js/modal-subscribe.js
@@ -6,17 +6,10 @@
     closeModalBtnSubscribe: document.querySelector(
       '[data-modal-close-Subscribe]'
     ),
-    closeOpenModalBtnSubscribe: document.querySelector(
-      '[data-modal-close-open-Subscribe]'
-    ),
     modalSubscribe: document.querySelector('[data-modal-Subscribe]'),
   };
   refs.openModalBtnSubscribe.addEventListener('click', toggleModalSubscribe);
   refs.closeModalBtnSubscribe.addEventListener('click', toggleModalSubscribe);
-  refs.closeOpenModalBtnSubscribe.addEventListener(
-    'click',
-    toggleModalSubscribe
-  );
 
   function toggleModalSubscribe() {
     refs.modalSubscribe.classList.toggle('is-hidden');
